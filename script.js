16324715811da287dc6e9153321107d2da2e09d5
Houser97 <arturo.riverar97@gmail.com>
2022-08-20 14:36:41 -0500

    Create class Node and arrays to use throughout the process

---
+++ script.js
@@ -0,0 +1,17 @@
+class Node{
+    constructor(x, y, parent = null){
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+    };
+};
+
+// Se crean los arreglos con los posibles movimientos de un caballo.
+let row = [2, 2, -2, -2, 1, 1, -1, -1];
+let col = [-1, 1, 1, -1, 2, -2, 2, -2];
+
+// Se crea el arreglo que contendrá a los nodos visitados.
+let visited = [];
+
+// Se crea el arreglo que actuará como QUEUE.
+let queue = [];
